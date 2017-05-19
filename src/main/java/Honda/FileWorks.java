92963e5c9aed1812d6d2898fc17c528a175740ac
dmtandrv <dmt.andrv@yandex.ru>
2017-05-19 16:34:10 +0300

    fileworks stuff

--- src/main/java/Honda/FileWorks.java
+++ src/main/java/Honda/FileWorks.java
@@ -7,34 +7,39 @@ import java.util.Collections;
 
 /**
  * Created by darts on 16.05.17.
- * 1 Writing random integers (0-100) to file,
- * 2 Writing them to arraylist
+ * 1 Writing 100 random integers (0-1000) to arraylist
+ * 2 Writing arraylist to file
  * 3 Sorting arraylist
- * 4 Writing arraylist to file
+ * 4 Writing sorted arraylist to file
  */
 public class FileWorks {
     public static void main(String[] args) throws IOException {
-        //random file stuff
-        FileWriter rndWriter = new FileWriter(System.getProperty("user.dir")+"/rndFile", false);
+
+        // 1 Writing random integers (0-100) to arraylist
         Random random= new Random();
         ArrayList<Integer> randomList = new ArrayList<Integer>();
-        for (int i=0; i<100; i++) {
-            int tempRnd = random.nextInt(100);
-            rndWriter.append(String.valueOf(tempRnd) + '\n');
-            randomList.add(tempRnd);
+
+        for (int i=0;i<100;i++) {
+            randomList.add(random.nextInt(1000));
+        }
+
+        // 2 Writing arraylist to file
+        FileWriter rndWriter = new FileWriter(System.getProperty("user.dir")+"/rndFile", false);
+        for(Object z : randomList){
+            rndWriter.append(String.valueOf(z)+ '\n');
         }
         rndWriter.flush();
 
+        // 3 Sorting arraylist
         Collections.sort(randomList,new Comparator<Integer>() {
                     public int compare(Integer o1, Integer o2) {
                         return o1.compareTo(o2);
                     }
         });
 
-        //sorted file stuff
+        // 4 Writing sorted arraylist to file
         FileWriter sortedWriter = new FileWriter(System.getProperty("user.dir")+"/sortedFile", false);
         for(Object z : randomList){
-            System.out.println(z);
             sortedWriter.append(String.valueOf(z)+ '\n');
         }
         sortedWriter.flush();
